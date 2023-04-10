758e4b55a513354f83d2d6d9a95a2b5bd617df15
Andrey Monchencko <monchenckoandrey@gmail.com>
2023-04-10 08:14:14 +0600

    scss added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// const doBidButton = document.querySelector('.dobid');
-
-// doBidButton.addEventListener('click', () => {
-//     doBidButton.classList.ad
-// })
+const headerButton = document.querySelector('.header-button');
+headerButton.addEventListener('click', () => {
+    headerButton.classList.toggle('header-button-active');
+});
+const doBidButton = document.querySelector('.dobid');
+doBidButton.addEventListener('click', () => {
+    doBidButton.classList.toggle('dobid-active');
+});
